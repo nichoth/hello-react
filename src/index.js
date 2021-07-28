589a26e439b898fe555e9689357a2a57bcd1f17a
nichoth <nichoth@gmail.com>
2021-07-28 15:55:58 -0700

    build demo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,53 @@
-var React = require('react');
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { html } from 'htm/react';
+var observ = require('observ')
+var struct = require('observ-struct')
+var Bus = require('@nichoth/events')
+var namespace = require('@nichoth/events/namespace')
 
-var el = html`<a href="/">Hello!</a>`
+var evs = namespace({
+    inc: ['one'],
+    minus: ['one']
+})
 
-ReactDOM.render(el, document.getElementById('content'));
+var bus = Bus({
+    memo: true
+})
+
+var state = struct({
+    count: observ(0)
+})
+
+bus.on(evs.inc.one, () => {
+    state.count.set(state.count() + 1)
+})
+
+bus.on(evs.minus.one, () => {
+    state.count.set(state.count() - 1)
+})
+
+function App (props) {
+    var [_state, setState] = useState(state());
+    state(function onChange (newState) {
+        setState(newState)
+    })
+    var emit = bus.emit.bind(bus)
+
+    return html`<div className="app">
+        <${Counter} ...${_state} emit=${emit} />
+    </div>`
+}
+
+function Counter (props) {
+    var { emit } = props
+    return html`<div className="counter">
+        ${props.count}
+        <div className="controls">
+            <button onClick=${emit(evs.inc.one)}>plus</button>
+            <button onClick=${emit(evs.minus.one)}>minus</button>
+        </div>
+    </div>`
+}
+
+ReactDOM.render(html`<${App} />`, document.getElementById('content'));
