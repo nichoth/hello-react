3da7c5864c2660e1b994b44e464e530bf4f01a52
nichoth <nichoth@gmail.com>
2021-07-28 13:02:46 -0700

    use babel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+var React = require('react');
 import ReactDOM from 'react-dom';
 import { html } from 'htm/react';
 
 var el = html`<a href="/">Hello!</a>`
 
-ReactDOM.render(el, document.body);
+ReactDOM.render(el, document.getElementById('content'));
